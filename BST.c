daf975a6bb67a4d325bfc0ac1ab6d393e2f776fc
Aarya Chopkar <101868279+accodes21@users.noreply.github.com>
2022-08-23 21:41:19 +0530

    Binary Search Tree

---
+++ BST.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Binary Search Tree operations in C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int key;
+  struct node *left, *right;
+};
+
+// Create a node
+struct node *newNode(int item) {
+  struct node *temp = (struct node *)malloc(sizeof(struct node));
+  temp->key = item;
+  temp->left = temp->right = NULL;
+  return temp;
+}
+
+// Inorder Traversal
+void inorder(struct node *root) {
+  if (root != NULL) {
+    // Traverse left
+    inorder(root->left);
+
+    // Traverse root
+    printf("%d -> ", root->key);
+
+    // Traverse right
+    inorder(root->right);
+  }
+}
+// Insert a node
+struct node *insert(struct node *node, int key) {
+  // Return a new node if the tree is empty
+  if (node == NULL) return newNode(key);
+
+  // Traverse to the right place and insert the node
+  if (key < node->key)
+    node->left = insert(node->left, key);
+  else
+    node->right = insert(node->right, key);
+
+  return node;
+}
+
+// Find the inorder successor
+struct node *minValueNode(struct node *node) {
+  struct node *current = node;
+
+  // Find the leftmost leaf
+  while (current && current->left != NULL)
+    current = current->left;
+
+  return current;
+}
+
+int main() {
+  struct node *root = NULL;
+  root = insert(root, 100);
+  root = insert(root, 300);
+  root = insert(root, 250);
+  root = insert(root, 400);
+  root = insert(root, 450);
+
+  printf("Inorder traversal: ");
+  inorder(root);
+
+//  printf("Inorder traversal: ");
+//  inorder(root);
+}
