aee9123d42ca4799e28e4a1b7168ee96d9cb7234
Aarya Chopkar <101868279+accodes21@users.noreply.github.com>
2023-10-21 18:27:27 +0530

    even odd linked list

---
+++ evenOddLL.cpp
@@ -0,0 +1,27 @@
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution{
+    public:
+        ListNode* oddEvenList(ListNode* head){
+            if(head == NULL) return head;
+
+            ListNode* odd = head;
+            ListNode* even = odd->next;
+            ListNode* evenHead = even;
+
+            while(odd!=NULL && even!=NULL){
+                odd->next = odd->next->next;
+                even->next = even->next->next;
+                odd = odd->next;
+                even = even->next;
+            }
+            odd->next = evenHead;
+            return head;
+        }
+}
